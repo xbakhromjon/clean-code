9ae0e7ff673b165ffbb674a04dbd83e76c618c4f
xbakhromjon <xbakhromjon@gmail.com>
2022-12-12 22:17:22 +0500

    init

---
+++ src/main/java/uz/bakhromjon/Main.java
@@ -0,0 +1,12 @@
+package uz.bakhromjon;
+
+/**
+ * @author : Bakhromjon Khasanboyev
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ src/main/java/uz/bakhromjon/meaningfulNames/MeaningfulNames.java
@@ -0,0 +1,65 @@
+package uz.bakhromjon.meaningfulNames;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : Bakhromjon Khasanboyev
+ **/
+public class MeaningfulNames {
+    public static void main(String[] args) {
+        int d; // elapsed time in days
+
+        int elapsedTimeInDays;
+        int daysSinceCreation;
+        int daysSinceModification;
+        int fileAgeInDays;
+
+
+    }
+
+    // bad code
+    List<int[]> theList = new ArrayList<>();
+
+    public List<int[]> getThem() {
+        List<int[]> list1 = new ArrayList<int[]>();
+        for (int[] x : theList)
+            if (x[0] == 4)
+                list1.add(x);
+        return list1;
+    }
+
+    // good code
+    List<int[]> gameBoard = new ArrayList<>();
+    final int STATUS_VALUE = 0;
+    final int FLAGGED = 4;
+
+//    public List<int[]> getFlaggedCells() {
+//        List<int[]> flaggedCells = new ArrayList<int[]>();
+//        for (int[] cell : gameBoard)
+//            if (cell[STATUS_VALUE] == FLAGGED)
+//                flaggedCells.add(cell);
+//        return flaggedCells;
+//    }
+
+
+    class Cell {
+        boolean isFlagged;
+        int[] arr;
+
+        public boolean isFlagged() {
+            return false;
+        }
+    }
+
+    List<Cell> gameBoardCell = new ArrayList<>();
+
+    public List<Cell> getFlaggedCells() {
+        List<Cell> flaggedCells = new ArrayList<Cell>();
+        for (Cell cell : gameBoardCell)
+            if (cell.isFlagged()) {
+                flaggedCells.add(cell);
+            }
+        return flaggedCells;
+    }
+}
