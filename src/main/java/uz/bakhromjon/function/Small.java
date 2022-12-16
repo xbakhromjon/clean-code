549be2096c1a5b449c53370cb15a9167ad0f209c
xbakhromjon <xbakhromjon@gmail.com>
2022-12-16 10:02:32 +0500

    functions

---
+++ src/main/java/uz/bakhromjon/function/Small.java
@@ -0,0 +1,17 @@
+package uz.bakhromjon.function;
+
+/**
+ * @author : Bakhromjon Khasanboyev
+ * @since : 16/12/22, Fri
+ **/
+public class Small {
+    public static void main(String[] args) {
+
+    }
+//    public static String renderPageWithSetupsAndTeardowns(
+//            PageData pageData, boolean isSuite) throws Exception {
+//        if (isTestPage(pageData))
+//            includeSetupAndTeardownPages(pageData, isSuite);
+//        return pageData.getHtml();
+//    }
+}
