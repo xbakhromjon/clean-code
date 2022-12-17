982947930e6314b7b25c47bbd8c6c04f9e87feee
xbakhromjon <xbakhromjon@gmail.com>
2022-12-17 14:20:09 +0500

    functions

---
+++ src/main/java/uz/bakhromjon/function/FlagArguments.java
@@ -0,0 +1,11 @@
+package uz.bakhromjon.function;
+
+/**
+ * @author : Bakhromjon Khasanboyev
+ * @since : 17/12/22, Sat
+ **/
+public class FlagArguments {
+    public static void main(String[] args) {
+
+    }
+}
